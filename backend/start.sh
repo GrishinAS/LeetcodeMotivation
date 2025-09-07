fa9401b6d648178c74b823445734103ca93591e9
GrishinAS <grishin.anton95@gmail.com>
2025-09-07 12:33:11 -0700

    Configs trims

--- backend/start.sh
+++ backend/start.sh
@@ -6,7 +6,7 @@ export DB_URL=
 export SPRING_PROFILES_ACTIVE=prod
 ./gradlew bootJar
 
-java -Duser.timezone=PST8 -jar ./build/libs/leetcode-motivation-service-0.1.jar &
+java -Xmx500m -Duser.timezone=PST8 -jar ./build/libs/leetcode-motivation-service-0.1.jar &
 APP_PID=$!
 
 echo $APP_PID > java_pid
